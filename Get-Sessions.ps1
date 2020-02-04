e3efd3c1ee8b9a1a74dbc814db9b934e5c44dabf
Simon Brown <simon.brown@bridgepartners.co.uk>
2020-02-04 08:01:14 +0000

    Resolved failure to create the properties ID and LogonTime

--- Get-Sessions.ps1
+++ Get-Sessions.ps1
@@ -11,20 +11,33 @@ Function Get-Sessions {
     $sessions | ForEach-Object {
         IF ($i -ne 0) {
             $_ -replace "Disc","Disconnected" -match '^[\s>]([\w\.]+)\s+(\brdp-tcp#\b\d\d|\bconsole\b|\s)\s+(\d+)\s+(\w+)\s+([\d+:\.|\bnone\b]+)\s+([\d\/\s:]+)$' | Out-Null
-            $Report = "" | Select-Object ComputerName,UserName,SessionName,ID,State,IdleTime,LogonTime
+            #$Report = "" | Select-Object ComputerName,UserName,SessionName,ID,State,IdleTime,LogonTime
+            $Report = New-Object psobject
             IF ($ComputerName) {
-                $Report.ComputerName = $ComputerName
+                # $Report.ComputerName = $ComputerName
+                Add-Member -InputObject $Report -MemberType NoteProperty -Name "ComputerName" -Value $ComputerName
             } ELSE {
-                $Report.ComputerName = "localhost"
+                # $Report.ComputerName = "localhost"
+                Add-Member -InputObject $Report -MemberType NoteProperty -Name "ComputerName" -Value "localhost"
             }
-            $Report.UserName = $matches[1]
-            $Report.SessionName = $matches[2]
-            $Report.ID = $matches[3]
-            $Report.State = $matches[4]
+            # $Report.UserName = $matches[1]
+            Add-Member -InputObject $Report -MemberType NoteProperty -Name "UserName" -Value $matches[1]
+            # $Report.SessionName = $matches[2]
+            Add-Member -InputObject $Report -MemberType NoteProperty -Name "SessionName" -Value $matches[2]
+            # $Report.ID = [int]($matches[3])
+            Add-Member -InputObject $Report -MemberType NoteProperty -Name "ID" -Value ([int]($matches[3]))
+            # $Report.State = $matches[4]
+            Add-Member -InputObject $Report -MemberType NoteProperty -Name "State" -Value $matches[4]
             IF ($matches[5] -ne "none") {
-                $Report.IdleTime = $matches[5] -replace "\+"," Days, " -replace ":"," Hours and " -replace '$'," Minutes"
+                # $Report.IdleTime = $matches[5] -replace "\+"," Days, " -replace ":"," Hours and " -replace '$'," Minutes"
+                Add-Member -InputObject $Report -MemberType NoteProperty -Name "IdleTime" -Value ($matches[5] -replace "\+"," Days, " -replace ":"," Hours and " -replace '$'," Minutes")
             }
-            $Report.LogonTime = $matches[6]
+            # $Report.LogonTime = $matches[6]
+            $CultureDateTimeFormat = (Get-Culture).DateTimeFormat
+            $DateFormat = $CultureDateTimeFormat.ShortDatePattern
+            $TimeFormat = $CultureDateTimeFormat.ShortTimePattern
+            $DateTimeFormat = "$DateFormat $TimeFormat"
+            Add-Member -InputObject $Report -MemberType NoteProperty -Name "LogonTime" -Value ([datetime]::ParseExact($matches[6],$DateTimeFormat,[System.Globalization.DateTimeFormatInfo]::InvariantInfo,[System.Globalization.DateTimeStyles]::None))
             $Report
         }
         $i++
